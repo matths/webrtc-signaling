993d5bace15ce2a06ad8af331c64037958830d42
Matthias Dittgen <matthias.dittgen@valiton.com>
2018-10-27 20:36:10 +0200

    Use ipv4 instead of mac addresses for naming known devices

--- config/knownDevices.js
+++ config/knownDevices.js
@@ -1,11 +1,21 @@
 var exports = module.exports = knownDevices = [{
-	"name": "sun",
-	"macAddress": "00:00:00:00:00:01",
-	"IPv4": "0.0.0.1",
-	"IPv6": "0000:0000:0000:0000:0001"
+	"name": "london",
+	"macAddress": "d0:f8:8c:ee:ce:ad",
+	"ipv4": "192.168.178.32",
+	"ipv6": "fe80:d2f8:8cff:feee:cead"
 }, {
-	"name": "moon",
-	"macAddress": "00:00:00:00:00:02",
-	"IPv4": "0.0.0.2",
-	"IPv6": "0000:0000:0000:0000:0002"
+	"name": "berlin",
+	"macAddress": "60:03:08:a8:a1:3e",
+	"ipv4": "192.168.178.27",
+	"ipv6": "fe80:1c19:7342:6b4f:b032"
+}, {
+	"name": "paris",
+	"macAddress": "d0:f8:8c:ee:ce:af",
+	"ipv4": "192.168.178.81",
+	"ipv6": "fe80:1c19:7342:6b4f:b032"
+}, {
+	"name": "local",
+	"macAddress": "00:00:00:00:00:00",
+	"ipv4": "127.0.0.1",
+	"ipv6": "fe80::1"
 }];
