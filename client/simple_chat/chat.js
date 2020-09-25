83fd0ac85654b6e1e56f54edb8f2656422843c80
Matthias Dittgen <dittgen@gmail.com>
2020-09-25 17:57:03 +0200

    Style chat

--- client/simple_chat/chat.js
+++ client/simple_chat/chat.js
@@ -1,17 +1,37 @@
-
+const channel = document.getElementById('channel');
 const nickname = document.getElementById('nickname');
 const message = document.getElementById('message');
 const messages = document.getElementById('messages');
+const startBtn = document.getElementById('start');
 const sendBtn = document.getElementById('send');
 
+var signaling;
+
 function sendMessage () {
-    if (!message.value) return;
-    channel.send({
+    var msg = message.value;
+    if (!msg) return;
+
+    signaling.send({
         type: 'chat',
         nickname: nickname.value?nickname.value:'anonymous',
-        message: message.value
+        message: msg
     });
+
+    message.value = '';
 };
+
+function start () {
+    var type = channel.selectedOptions[0].value;
+    var host = window.location.host;
+    signaling = SignalingChannel.getChannel(host, type, channelOpened);
+    channel.disabled = true;
+    nickname.disabled = true;
+    startBtn.disabled = true;
+    message.disabled = false;
+    sendBtn.disabled = false;
+}
+
+startBtn.addEventListener('click', start);
 
 sendBtn.addEventListener('click', sendMessage);
 message.addEventListener('keyup', function (e) {
@@ -20,18 +40,17 @@ message.addEventListener('keyup', function (e) {
     }
 });
 
-function channelOpened (channel) {
-    channel.addEventListener('message', function (e) {
+function channelOpened (signaling) {
+    signaling.addEventListener('message', function (e) {
         var div = document.createElement('div');
         div.innerHTML = '<span class="nickname">'+e.message.nickname+':</span> <span class="message">'+e.message.message+'</span>';
-        messages.insertBefore(div, messages.firstChild);
+        // messages.insertBefore(div, messages.firstChild);
+        messages.append(div);
+        messages.scrollTop = messages.scrollHeight - messages.clientHeight;
 
         while (messages.children.length > 20) {
-            messages.lastElementChild.remove();
+            // messages.lastElementChild.remove();
+            messages.firstElementChild.remove();
         }
     });
 };
-
-var host = window.location.host;
-//var channel = new XhrChannel(host, channelOpened);
-var channel = new WsChannel(host, channelOpened);
